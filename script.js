3574a4e78974e7f245b0ee280d8111f43b64067d
NahuelRosso <nahuelgrosso25@gmail.com>
2023-10-10 20:20:10 -0300

    prueba de errores

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Espera a que el documento esté listo
-$(document).ready(function() {
-    // Selecciona el contenedor del carrusel y llama a la función slick()
-    $('.carousel').slick({
-        autoplay: true,
-        dots: true,
-        arrows: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-});
+// // Espera a que el documento esté listo
+// $(document).ready(function() {
+//     // Selecciona el contenedor del carrusel y llama a la función slick()
+//     $('.carousel').slick({
+//         autoplay: true,
+//         dots: true,
+//         arrows: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//     });
+// });
